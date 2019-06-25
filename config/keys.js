61707b791557fff3edd87d8d7722af718a697f28
Waseem Aslam <waseemaslam467@gmail.com>
2019-06-25 21:15:25 +0530

    node-passport-jwt_boilerplate

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://waseem467:waseem467@ds341847.mlab.com:41847/mydevconnector"
+    "mongodb://waseem467:waseem467@ds341847.mlab.com:41847/mydevconnector",
+  secretOrKey: "secret"
 };
